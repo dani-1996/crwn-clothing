3835d5f64a9fe58b61288f3f8db81281b52567b5
Daniel <danielsteinhauser1906@gmail.com>
2021-11-05 22:26:05 +0100

    implement Redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Link } from "react-router-dom"
 import {auth} from "../../firebase/firebase.utils"
+import {connect} from "react-redux"
+
 
 
 import "./header.style.scss"
@@ -31,4 +33,8 @@ const Header = ({currentUser}) => {
     )
 }
 
-export default Header
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header) 
